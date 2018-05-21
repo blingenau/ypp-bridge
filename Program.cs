0bb8ef260a1be11cd5bf0b748cf7bf156deeb322
yyuuii <yppseabattle@gmail.com>
2018-05-20 21:17:45 -0400

    Added input stream loop

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
+using Newtonsoft.Json;
 using WindowsInput.Native;
 using WindowsInput;
 
@@ -12,6 +16,7 @@ namespace quartermaster
     class Program
     {
         static Process yppProcess;
+        static StreamWriter w = File.AppendText(@"C:\Users\Blake\test\log.txt");
 
         [DllImport("user32.dll")]
         [return: MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
@@ -59,7 +64,13 @@ namespace quartermaster
             sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
         }
 
-        static public void GetPirateInfo() {
+        static public void SendMessageToQM(Dictionary<string, string> response) {
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+            Console.Out.Flush();
+        }
+
+        static public Dictionary<string, string> GetPirateInfo() {
+            var response = new Dictionary<string, string>();
             string title = yppProcess.MainWindowTitle;
             Regex pirateInfo = new Regex(@"(\w+) on the (\w+) ocean");
             Match match = pirateInfo.Match(title);
@@ -69,10 +80,12 @@ namespace quartermaster
                 // The Pirate name (i.e. "Yyuuii") at position 1
                 // The Ocean name (i.e. "Obsidian") at position 1
                 if (match.Groups.Count == 3) {
-                    string pirateName = match.Groups[1].Value;
-                    string oceanName = match.Groups[2].Value;
+                    response.Add("pirate", match.Groups[1].Value);
+                    response.Add("ocean", match.Groups[2].Value);
+                    return response;
                 }
             }
+            throw new Exception("Couldn't get pirate info.");
         }
 
         static public Process GetYppProcess() {
@@ -84,22 +97,70 @@ namespace quartermaster
             {
                 return bProcess;
             } else {
-                throw new System.Exception("Process not running");
+                throw new Exception("Process not running");
             }
+        }
+
+        static public void HandleError(Exception e) {
+            Dictionary<string, string> response = new Dictionary<string, string>();
+            response.Add("Type", "Error");
+            response.Add("Data", e.Message);
+            SendMessageToQM(response);
         }
 
         static int Main(string[] args)
         {
-            // Cache the running ypp process
-            
+            // Cache the running ypp process            
             try
             {
                 yppProcess = GetYppProcess();
             }
-            catch (System.Exception)
+            catch (Exception e)
             {
                 return 1;
             }
+
+            string line = null;
+            do
+            {
+                line = Console.ReadLine();
+                try
+                {
+                    var input = JsonConvert.DeserializeObject<Dictionary<string, string>>(line);
+                    var action = input["Action"];
+                    Dictionary<string, string> response = new Dictionary<string, string>();
+                    switch (action)
+                    {
+                        case "send":
+                            var message = input["Data"];
+                            SendMessageToYPP(message);
+                            break;
+                        case "getPirateInfo":
+                            Dictionary<string, string> data;
+                            try
+                            {
+                                data = GetPirateInfo();
+                            }
+                            catch (Exception e)
+                            {
+                                HandleError(e);
+                                break;
+                            }
+                            response.Add("Type", "PirateInfo");
+                            response.Add("Data", JsonConvert.SerializeObject(data));
+                            SendMessageToQM(response);
+                            break;
+                        default:
+                            Console.WriteLine("Action not found");
+                            break;
+                    }
+                    line = null;
+                }
+                catch (Exception e)
+                {
+                    HandleError(e);
+                }
+            } while (line == null);
 
             return 0;
         }
